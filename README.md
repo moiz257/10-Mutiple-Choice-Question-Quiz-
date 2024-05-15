2df8de1d1269ea1b8ecf6c681da487fb2bba6184
72737865 <156658135+72737865@users.noreply.github.com>
2024-05-15 22:01:50 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# 10-Mutiple-Choice-Question-Quiz-" 
---
+++ main.js
@@ -0,0 +1,39 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.bold.cyanBright("          MULTIPLE CHOICE QUESTION  'QUIZ'          \n"));
+const apiLink = "https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple";
+let fetchData = async (data) => {
+    let fetchQuiz = await fetch(data);
+    let quizQ = await fetchQuiz.json();
+    return quizQ.results;
+};
+let quiz = await fetchData(apiLink);
+let startQuiz = async () => {
+    let score = 0;
+    // for username
+    let nameAnswer = await inquirer.prompt({
+        name: "question1",
+        message: chalk.bold.hex("#e6b209")("\nFIRST. ENTER YOUR NAME!"),
+        type: "input",
+        validate: function (value) {
+            if (value.trim() !== "")
+                return true;
+            return chalk.underline.redBright("\n ENTER A VALID NAME!!");
+        }
+    });
+    for (let i = 1; i < 10; i++) {
+        let answer = [...quiz[i].incorrect_answers, quiz[i].correct_answer];
+        let ans = await inquirer.prompt({
+            name: "ques",
+            type: "list",
+            message: quiz[i].question,
+            choices: answer.map((val) => val)
+        });
+        if (ans.ques == quiz[i].correct_answer) {
+            ++score;
+        }
+    }
+    console.log(chalk.bold.hex("#e6b209")(` CORRECT ANSWER.${score}`), (chalk.bold.hex("#e6b209")("\nMULTIPLE CHOICE QUESTION  'QUIZ' IS END")));
+};
+startQuiz();
